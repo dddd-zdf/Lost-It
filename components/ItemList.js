3971543f3b98530d7c38feab196f99374b952c2c
difeizhao <zhao.dif@northeastern.edu>
2023-03-26 15:20:37 -0700

    filter for ItemList
    to distinguish item for all or item under user profile

--- components/ItemList.js
+++ components/ItemList.js
@@ -1,10 +1,16 @@
 import * as React from 'react';
 import { Text, View, Pressable } from 'react-native';
 
-export default function ItemList({navigation}) {
+export default function ItemList({route},{navigation}) {
+
+  const filter = route.params.filter;
   return (
     <View>
-      <Text>ItemList Screen</Text>
+      <Text>
+        {
+          filter == 'user' ? ('item list under a user profile') : ('item list for all items')
+        }
+      </Text>
       <Pressable
         onPress={() => navigation.navigate('Item Details')}
         style={({ pressed }) => [
