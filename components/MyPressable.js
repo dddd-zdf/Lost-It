6788acdffacfbc9238e02fce91ff9c5d89c9a347
Aly Badary <97933619+AlyATB@users.noreply.github.com>
2023-03-27 16:58:37 -0700

    updated MyPressable

--- components/MyPressable.js
+++ components/MyPressable.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Pressable } from "react-native";
+import React from "react";
 
-export default function MyPressable() {
-  return (
-    <View>
-      <Text>MyPressable</Text>
-    </View>
-  )
+export default function MyPressable({
+    children,
+    pressedFunction,
+    customStyle,
+    pressedStyle,
+}) {
+    return (
+        <Pressable
+            onPress={pressedFunction}
+            style={({ pressed }) => {
+                return [customStyle, pressed && pressedStyle];
+            }}
+        >
+            {children}
+        </Pressable>
+    );
 }
