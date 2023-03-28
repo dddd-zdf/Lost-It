fc3cb787357f1ae42b728f62e91cf00687d25ec1
Aly Badary <97933619+AlyATB@users.noreply.github.com>
2023-03-27 17:59:30 -0700

    connected db to Home screen

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,14 +1,44 @@
-import * as React from "react";
 import { Text, View, Pressable } from "react-native";
 import { ScreenContainer } from "../helper";
 import EntriesList from "../components/EntriesList";
+import React, { useState, useEffect } from "react";
+import { firestore } from "../Firebase/firebase-setup";
+import { onSnapshot, collection } from "firebase/firestore";
 
 export default function Home({ navigation }) {
     // const filter = route.params.filter;
-    entries = [
-        { title: "AirPods", description: "Brand New" },
-        { title: "KeyChain", description: "Kamado Tanjiro DS" },
-    ];
+    // entries = [
+    //     { title: "AirPods", description: "Brand New" },
+    //     { title: "KeyChain", description: "Kamado Tanjiro DS" },
+    // ];
+
+    const [entries, setEntries] = useState([]);
+    function onEntryPress(entry) {
+        navigation.navigate("Item Details", entry);
+    }
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+            collection(firestore, "entries"),
+            (querySnapshot) => {
+                if (querySnapshot.empty) {
+                    setEntries([]);
+                } else {
+                    let docs = [];
+                    querySnapshot.docs.forEach((document) => {
+                        return docs.push({
+                            ...document.data(),
+                            key: document.id,
+                        });
+                    });
+                    setEntries(docs);
+                }
+            }
+        );
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
     function onEntryPress() {
         console.log("Clicked Entry");
@@ -19,5 +49,3 @@ export default function Home({ navigation }) {
         </View>
     );
 }
-
-
