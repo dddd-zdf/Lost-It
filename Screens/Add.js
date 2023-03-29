ceed7586c508d09011c6a45be527291c90ed35c1
Aly Badary <97933619+AlyATB@users.noreply.github.com>
2023-03-28 17:47:58 -0700

    added MyInput and integrated it into Add screen

--- Screens/Add.js
+++ Screens/Add.js
@@ -1,10 +1,74 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import MyInput from "../components/MyInput";
+import { COLORS, ScreenContainer } from "../helper";
+import MyPressable from "../components/MyPressable";
 
 export default function Add() {
-  return (
-    <View>
-      <Text>Add Screen</Text>
-    </View>
-  );
-};
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    function resetInputs() {
+        setTitle("");
+        setDescription("");
+    }
+
+    return (
+        <View style={[ScreenContainer, { paddingTop: 50 }]}>
+            <Text>Add Screen</Text>
+            <MyInput
+                inputName={"Title"}
+                value={title}
+                textUpdateFunction={setTitle}
+            />
+            <MyInput
+                inputName={"Description"}
+                value={description}
+                textUpdateFunction={setDescription}
+                customStyle={{ height: 100 }}
+            />
+
+            <View style={styles.pressablesContainer}>
+                <MyPressable
+                    pressedFunction={() => resetInputs()}
+                    customStyle={styles.pressable}
+                    pressedStyle={{ opacity: 0.8 }}
+                >
+                    <Text style={styles.text}>Reset</Text>
+                </MyPressable>
+                <MyPressable
+                    pressedFunction={() => onSubmit(calories, description)}
+                    customStyle={styles.pressable}
+                    pressedStyle={{ opacity: 0.5 }}
+                >
+                    <Text style={styles.text}>Submit</Text>
+                </MyPressable>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    pressablesContainer: {
+        flexDirection: "row",
+        marginTop: 25,
+        width: 260,
+        height: 40,
+        borderColor: "black",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    pressable: {
+        flex: 1,
+        alignItems: "center",
+        marginHorizontal: 10,
+        paddingVertical: 7,
+        borderRadius: 3,
+        backgroundColor: COLORS.BLUE,
+    },
+    text: {
+        fontSize: 13,
+        color: COLORS.WHITE,
+        fontWeight: "500",
+    },
+});
--- helper.js
+++ helper.js
@@ -5,11 +5,13 @@ export const COLORS = {
     GOLD: "#F1C80D",
     LIGHTBLUE: "#A9CCE3",
     BLACK: "black",
+    MAROON: "#800000",
+    CYAN2: "#a1bbc4"
 };
 
 export const ScreenContainer = {
     flex: 1,
-    backgroundColor: COLORS.CYAN,
+    backgroundColor: COLORS.CYAN2,
     justifyContent: "flex-start",
     alignItems: "center",
 };
