8f4a18b3d3b31a9caf867e6d97999e4b07b45303
ziasyed9 <9ziasyed@gmail.com>
2023-03-29 21:21:27 -0700

    Added the edit page components, and cancel button functionality

--- Screens/Edit.js
+++ Screens/Edit.js
@@ -1,15 +1,74 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React, { useState } from "react";
+import { COLORS, ScreenContainer } from "../helper";
+import MyInput from "../components/MyInput";
+import MyPressable from "../components/MyPressable";
 
-export default function Edit({ route }) {
-    const { title, description, key } = route.params;
+export default function Edit({ route, navigation }) {
+  const { title, description, key } = route.params;
 
-    const [updatedTitle, setUpdatedTitle] = useState(title);
-    const [updatedDescription, setUpdatedDescription] = useState(description);
+  const [updatedTitle, setUpdatedTitle] = useState(title);
+  const [updatedDescription, setUpdatedDescription] = useState(description);
 
-    return (
-        <View>
-            <Text>{title}</Text>
-        </View>
-    );
+  function cancel() {
+    return navigation.goBack();
+  }
+
+  return (
+    <View style={[ScreenContainer, { paddingTop: 50 }]}>
+      <MyInput
+        inputName={"Title"}
+        value={title}
+        textUpdateFunction={setUpdatedTitle}
+      />
+      <MyInput
+        inputName={"Description"}
+        value={description}
+        textUpdateFunction={setUpdatedDescription}
+        customStyle={{ height: 100 }}
+      />
+
+      <View style={styles.pressablesContainer}>
+        <MyPressable
+          pressedFunction={() => cancel()}
+          customStyle={styles.pressable}
+          pressedStyle={{ opacity: 0.8 }}
+        >
+          <Text style={styles.text}>Cancel</Text>
+        </MyPressable>
+        <MyPressable
+          pressedFunction={() => onSubmit(title, description)}
+          customStyle={styles.pressable}
+          pressedStyle={{ opacity: 0.5 }}
+        >
+          <Text style={styles.text}>Submit</Text>
+        </MyPressable>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  pressablesContainer: {
+    flexDirection: "row",
+    marginTop: 25,
+    width: 260,
+    height: 40,
+    borderColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  pressable: {
+    flex: 1,
+    alignItems: "center",
+    marginHorizontal: 10,
+    paddingVertical: 7,
+    borderRadius: 3,
+    backgroundColor: COLORS.BLUE,
+  },
+  text: {
+    fontSize: 13,
+    color: COLORS.WHITE,
+    fontWeight: "500",
+  },
+});
