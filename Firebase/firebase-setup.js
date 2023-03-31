da9d2b55271b5f7951a8d89ac79192a8e850addf
Aly Badary <97933619+AlyATB@users.noreply.github.com>
2023-03-31 15:30:38 -0700

    added .env

--- Firebase/firebase-setup.js
+++ Firebase/firebase-setup.js
@@ -3,18 +3,25 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import {
+    apiKey,
+    authDomain,
+    projectId,
+    storageBucket,
+    messagingSenderId,
+    appId,
+} from "@env";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqfNNH9jlK6e6QPg0i_83clVRtrBO4iC4",
-  authDomain: "lostit-dd4db.firebaseapp.com",
-  projectId: "lostit-dd4db",
-  storageBucket: "lostit-dd4db.appspot.com",
-  messagingSenderId: "25896468303",
-  appId: "1:25896468303:web:cd2a95bf227e90c32c0f3a"
+    apiKey: apiKey,
+    authDomain: authDomain,
+    projectId: projectId,
+    storageBucket: storageBucket,
+    messagingSenderId: messagingSenderId,
+    appId: appId,
 };
 
 // Initialize Firebase
 const myApp = initializeApp(firebaseConfig);
 export const firestore = getFirestore(myApp);
-
